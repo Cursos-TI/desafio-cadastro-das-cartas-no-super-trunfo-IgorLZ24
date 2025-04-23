f116cb7764d3047e87537b0a0cd6f74dff63c62c
IgorLZ24 <igor.luiz.bdc@gmail.com>
2025-04-23 03:29:49 +0000

    Jogo CartasSuperTrunfo NIVEL MESTRE, FINALIZADO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,7 +11,8 @@ int main()
     float PIB_carta1, PIB_carta2;
     float Densidade_Populacional1, Densidade_Populacional2;
     float PIB_perCapita1, PIB_perCapita2;
-
+    float SuperPoderCarta1, SuperPoderCarta2;
+    int Resultado1, Resultado2, Resultado3, Resultado4, Resultado5, Resultado6, Resultado7;
     //Dados da Carta 1 abaixo
     printf("Digite a inicial de A a H do seu estado: \n");
     scanf("%s", estado_carta1);
@@ -37,6 +38,8 @@ int main()
     Densidade_Populacional1 = (float)(populacao_carta1 / area_da_cidade_carta1);
 
     PIB_perCapita1 = (float)(populacao_carta1 / PIB_carta1);
+
+    SuperPoderCarta1 = (populacao_carta1 + area_da_cidade_carta1 + PIB_carta1 + PIB_perCapita1 + numero_de_pontosTuristicos_carta1 +(1/Densidade_Populacional1));
 
     //Dados da Carta 2 abaixo
     printf("Digite a inicial de A a H do seu estado (carta 2): \n");
@@ -64,6 +67,8 @@ int main()
 
     PIB_perCapita2 = (float)(populacao_carta2 / PIB_carta2);
 
+    SuperPoderCarta2 = (populacao_carta2 + area_da_cidade_carta2 + PIB_carta2 + PIB_perCapita2 + numero_de_pontosTuristicos_carta2 +(1/Densidade_Populacional2));
+
     // Exibindo os dados da carta 1
     printf("CARTA 1: \n");
     printf("Estado: %s\n", estado_carta1);
@@ -88,6 +93,25 @@ int main()
     printf("Densidade populacional é: %.2f hab/km²\n", Densidade_Populacional2);
     printf("PIB per capita: %.2f reais\n", PIB_perCapita2);
 
+    // Comparando os dados das cartas e ver qual vence
+
+    Resultado1 = populacao_carta1 > populacao_carta2;
+    Resultado2 = area_da_cidade_carta1 > area_da_cidade_carta2;
+    Resultado3 = PIB_carta1 > PIB_carta2;
+    Resultado4 = numero_de_pontosTuristicos_carta1 > numero_de_pontosTuristicos_carta2;
+    Resultado5 = (1/Densidade_Populacional1) > (1/Densidade_Populacional2);
+    Resultado6 = PIB_perCapita1 > PIB_perCapita2;
+    Resultado7 = SuperPoderCarta1 > SuperPoderCarta2;
+
+    printf("se for 0 a carta escolhida perdeu, se for 1 a carta escolhida ganhou(neste caso comparando os atriputos da Carta 1 com os da Carta 2).\n");
+    printf("População(carta1): (%d)\n", Resultado1);
+    printf("Área(carta1): (%d)\n", Resultado2);
+    printf("PIB(carta1): (%d)\n", Resultado3);
+    printf("Numero de Pontos Turísticos(carta1): (%d)\n", Resultado4);
+    printf("Densidade Populacional(carta1): (%d)\n", Resultado5);
+    printf("PIB per Capita(carta1): (%d)\n", Resultado6);
+    printf("Super Poder(carta1): (%d)\n", Resultado7);
+    
     return 0;
 
 }
